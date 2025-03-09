aee7a045f3c6d7d8011a7448a912e90e2d2507c5
muthiazzh <muthiahazzahra200@gmail.com>
2025-03-09 18:26:50 +0700

    menambahkan implementasi dengan state-based construction

--- TPMODUL4_103022330117/Program.cs
+++ TPMODUL4_103022330117/Program.cs
@@ -1,33 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-public class KodePos
+//public class KodePos
+//{
+
+//    private static Dictionary<string, string> kodePosTable = new Dictionary<string, string>
+//    {
+//        { "Batununggal", "40266" },
+//        { "Kujangsari", "40287" },
+//        { "Mengger", "40267" },
+//        { "Wates", "40256" },
+//        { "Cijaura", "40287" },
+//        { "Jatisari", "40286" },
+//        { "Margasari", "40286" },
+//        { "Sekejati", "40286" },
+//        { "Kebonwaru", "40272" },
+//        { "Maleer", "40274" },
+//        { "Samoja", "40273" }
+//    };
+
+//    public static string GetKodePos(string kelurahan)
+//    {
+//        if (kodePosTable.ContainsKey(kelurahan))
+//        {
+//            return kodePosTable[kelurahan];
+//        }
+//        else
+//        {
+//            return "Kode pos tidak ditemukan";
+//        }
+//    }
+//}
+
+//public class Program
+//{
+//    public static void Main()
+//    {
+//        Console.Write("Masukkan nama kelurahan: ");
+//        string kelurahan = Console.ReadLine();
+
+//        string kodePos = KodePos.GetKodePos(kelurahan);
+
+//        Console.WriteLine($"Kode Pos untuk {kelurahan} adalah {kodePos}");
+//    }
+//}
+
+using System;
+
+public class DoorMachine
 {
+    public enum State { Terkunci, Terbuka }
 
-    private static Dictionary<string, string> kodePosTable = new Dictionary<string, string>
-    {
-        { "Batununggal", "40266" },
-        { "Kujangsari", "40287" },
-        { "Mengger", "40267" },
-        { "Wates", "40256" },
-        { "Cijaura", "40287" },
-        { "Jatisari", "40286" },
-        { "Margasari", "40286" },
-        { "Sekejati", "40286" },
-        { "Kebonwaru", "40272" },
-        { "Maleer", "40274" },
-        { "Samoja", "40273" }
-    };
+    private State currentState;
 
-    public static string GetKodePos(string kelurahan)
+    public DoorMachine()
     {
-        if (kodePosTable.ContainsKey(kelurahan))
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    public void HandleCommand(string command)
+    {
+        switch (currentState)
         {
-            return kodePosTable[kelurahan];
-        }
-        else
-        {
-            return "Kode pos tidak ditemukan";
+            case State.Terkunci:
+                if (command == "BukaPintu")
+                {
+                    currentState = State.Terbuka;
+                    Console.WriteLine("Pintu tidak terkunci");
+                }
+                break;
+
+            case State.Terbuka:
+                if (command == "KunciPintu")
+                {
+                    currentState = State.Terkunci;
+                    Console.WriteLine("Pintu terkunci");
+                }
+                break;
         }
     }
 }
@@ -36,11 +86,18 @@ public class Program
 {
     public static void Main()
     {
-        Console.Write("Masukkan nama kelurahan: ");
-        string kelurahan = Console.ReadLine();
+        DoorMachine pintu = new DoorMachine();
 
-        string kodePos = KodePos.GetKodePos(kelurahan);
+        while (true)
+        {
+            Console.Write("Enter Command: ");
+            string command = Console.ReadLine();
 
-        Console.WriteLine($"Kode Pos untuk {kelurahan} adalah {kodePos}");
+            if (command == "QUIT") break;
+
+            pintu.HandleCommand(command);
+        }
+
+        Console.WriteLine("Program selesai.");
     }
 }
